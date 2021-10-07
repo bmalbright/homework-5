fb9cc00db66158c38bedc16bf3599ac50ce4dcf0
bmalbright <brianalbright@utexas.edu>
2021-10-06 22:57:41 -0500

    last commit before publishing

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # homework-5
+The task was the create a daily planner that lets the user input tasks during the workday. Once submitted, the task would be stored in local storage and publish to the time block. Also, the background color of the time blocks is supposed to change as the day progesses. 
+
+Started code was given to us. I created a div to contain a time block, and copied and pasted it until I had enough. Then went to work on the js. I needed function to take the info submitted on the button click and put it in local storage. Then needed a funciton to take the task from local storage and publish it to the time block. Finally, I needed an if/else statement to compare the time associated with the time blocks to the current time, and change the background color of the time blocks as the day progressed. 
+
+I had some help from my tutor, Brian Ford, on the assignment. And I got a little help from the Ask BCS support in Slack on the if/else statement. No one else helped or saw my code. I did use moment.js. I looked up references on MDN, W3, etc, but I don't believe I used any code from those sources. 
+
--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
 
+// uses moment.js to set the day and put it at the top of the page
 var day = moment().format('MMMM Do YYYY');
 $("#currentDay").text(day)
 
-
+// when the submit button is clicked, the task the user inputed and stores it in local storage
 $(".saveBtn").click (
     function () {
         var task = $(this).siblings(".description").val()
@@ -12,6 +13,7 @@ $(".saveBtn").click (
     }
 )
 
+//takes the task from local storage and shows it on the page. 
 $(".time-block").each (function () {
     var timeId = $(this).attr("id")
     console.log(timeId)
@@ -19,13 +21,20 @@ $(".time-block").each (function () {
     var localTask = localStorage.getItem(timeId)
     $(this).children(".description").val(localTask)
 
-    var currentHour = moment.getHour;
-    console.log(currentHour)
+  //this section changes the background color of the time blocks as the day progresses. 
+   var currentHour = moment().hour();
+   console.log(currentHour)
 
     if (currentHour < timeId) {
-
-    }
-})
+            $(this).addClass("future");
+          } 
+          else if (currentHour == timeId) {
+            $(this).addClass("present");
+          } 
+          else if (currentHour > timeId) {
+            $(this).addClass("past");
+          }
+    })
 
 
 
